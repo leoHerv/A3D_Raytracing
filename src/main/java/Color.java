a8cd3d537852643c24a04a85e75a1691976c2e2c
Léo Hervouet <leoohervouet@gmail.com>
2024-05-19 22:27:51 +0200

    Added a basic Color class

---
+++ src/main/java/Color.java
@@ -0,0 +1,19 @@
+/**
+ *  Basic class to represent a Color.
+ *  @author HERVOUET Léo
+ *  @version 1.0
+ */
+public class Color
+{
+    public final double B;
+    public final double G;
+    public final double R;
+
+    public Color(double b, double g, double r)
+    {
+        B = b;
+        G = g;
+        R = r;
+    }
+
+}
