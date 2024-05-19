cf95c9cebd553a6e3c694365aa6d247348a780e4
Léo Hervouet <leoohervouet@gmail.com>
2024-05-19 23:15:02 +0200

    Added an alpha color in a Color

--- src/main/java/a3d/Color.java
+++ src/main/java/a3d/Color.java
@@ -10,12 +10,14 @@ public class Color
     public final double B;
     public final double G;
     public final double R;
+    public final double A;
 
-    public Color(double b, double g, double r)
+    public Color(double b, double g, double r, double a)
     {
         B = b;
         G = g;
         R = r;
+        A = a;
     }
 
 }
