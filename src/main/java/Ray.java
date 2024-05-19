f1bb98cf51649e1249fba849deafdf9b8bf4da4a
Léo Hervouet <leoohervouet@gmail.com>
2024-05-19 22:25:28 +0200

    Added a basic Ray class

---
+++ src/main/java/Ray.java
@@ -0,0 +1,21 @@
+/**
+ *  Basic class to represent a ray.
+ *  @author HERVOUET Léo
+ *  @version 1.0
+ */
+public class Ray
+{
+    private final Vec3D m_startingPoint;
+    private final Vec3D m_direction;
+
+    public Ray(Vec3D startingPoint, Vec3D direction)
+    {
+        m_startingPoint = startingPoint;
+        m_direction = direction;
+    }
+
+    public Ray(Vec3D direction)
+    {
+        this(new Vec3D(0, 0, 0), direction);
+    }
+}
