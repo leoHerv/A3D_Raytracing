81760164f129b84ce4238945f0ee3e998d803032
Léo Hervouet <leoohervouet@gmail.com>
2024-05-26 00:01:57 +0200

    Update the scene findColor process

--- src/main/java/a3d/scene/Scene.java
+++ src/main/java/a3d/scene/Scene.java
@@ -1,12 +1,14 @@
 package a3d.scene;
 
-        import a3d.Color;
-        import a3d.Ray;
-        import a3d.Vec3D;
-        import a3d.object.Object;
-        import a3d.object.sphere.light.Light;
+import a3d.Color;
+import a3d.Ray;
+import a3d.Vec3D;
+import a3d.object.Object;
+import a3d.object.sphere.light.Light;
 
-        import java.util.ArrayList;
+import java.util.ArrayList;
+
+import static a3d.Main.REFLECTION_NB;
 
 /**
  *  Basic class to represent a scene.
@@ -15,7 +17,7 @@ package a3d.scene;
  */
 public class Scene {
 
-    public static final int LEVEL_MAX = 10;
+    public static final int LEVEL_MAX = REFLECTION_NB;
     public static final double EPSILON = 0.001;
     public static final double SHADOW_COLOR_DIVIDER = 8;
     protected ArrayList<Object> m_objects;
@@ -59,24 +61,31 @@ public class Scene {
         {
             return new Color(0, 0, 0, 0);
         }
+        if(minObj instanceof Light)
+        {
+            return new Color(minObj.m_color, true);
+        }
 
         Vec3D intersectionPoint = Vec3D.add(ray.m_startingPoint, Vec3D.scale(ray.m_direction, minLambda));
-        Color color = Color.multiply(minObj.m_color, m_ambientLight);
+        Color colorIntersectionPoint = minObj.getColor(intersectionPoint);
+        Color color = Color.multiply(colorIntersectionPoint, m_ambientLight);
         boolean shadows = true;
 
         for(Light light : m_lights)
         {
             Vec3D lightDir = Vec3D.sub(light.m_position, intersectionPoint);
-            Ray shadingRay = new Ray(Vec3D.add(intersectionPoint, Vec3D.scale(lightDir, EPSILON)), lightDir);
+            Ray shadingRay = new Ray(Vec3D.add(intersectionPoint, Vec3D.scale(lightDir, EPSILON)), new Vec3D(lightDir));
             boolean visible = true;
 
             for(Object obj : m_objects)
             {
-                Double lambda = obj.computeIntersection(shadingRay);
-                if(lambda != null && (EPSILON < lambda && lambda < 1))
-                {
-                    visible = false;
-                    break;
+                if(!(obj instanceof Light) && minObj != obj){
+                    Double lambda = obj.computeIntersection(shadingRay);
+                    if(lambda != null && (EPSILON < lambda && lambda < 1))
+                    {
+                        visible = false;
+                        break;
+                    }
                 }
             }
 
@@ -87,17 +96,18 @@ public class Scene {
                 Vec3D intersectionPointNormal = Vec3D.normalize(intersectionPoint);
                 Vec3D lightDirNormal = Vec3D.normalize(lightDir);
 
-                color.add(Color.multiply(minObj.m_color, Color.multiply(light.m_color, objNormal.dotProduct(lightDirNormal))));
+                color.add(Color.multiply(colorIntersectionPoint, Color.multiply(light.m_color, objNormal.dotProduct(lightDirNormal))));
                 Vec3D h = Vec3D.sub(lightDirNormal, intersectionPointNormal);
-                double v = Math.max(objNormal.dotProduct(h), 0D);
+                double v = Math.pow(Math.max(objNormal.dotProduct(h), 0D), minObj.m_shininess);
                 color.add(Color.multiply(light.m_specular, Color.multiply(minObj.m_specular, v)));
 
-                // TODO Add recursion.
-                //Ray ref = new Ray(intersectionPoint, ray.m_direction - 2 * (objNormal.dotProduct(ray.m_direction)) objNormal);
-                //color.add(Color.multiply(minObj.m_coeffReflection, findColor(ref, level + 1));
+                if(minObj.m_coeffReflection > 0.0){
+                    Ray ref = new Ray(new Vec3D(intersectionPoint), Vec3D.sub(ray.m_direction, Vec3D.scale(objNormal, 2 * (objNormal.dotProduct(ray.m_direction)))));
+                    color.add(Color.multiply(findColor(ref, level + 1), minObj.m_coeffReflection));
+                }
             }
             else{
-                color.add(Color.multiply(minObj.m_color, Color.multiply(m_ambientLight,0.2))); // Add ambient shadow color
+                color.add(Color.multiply(colorIntersectionPoint, Color.multiply(m_ambientLight,0.2))); // Add ambient shadow color
             }
         }
 
@@ -106,6 +116,15 @@ public class Scene {
         }
 
         return color;
+    }
+
+    /** Add the light to the objects and lights lists.
+     *  @param light The light to add to the scene.
+     */
+    public void addLight(Light light)
+    {
+        m_objects.add(light);
+        m_lights.add(light);
     }
 
 }
